5cbbb850e602cd2983cfb096b1b2da8bdeba4a1e
hawk0616 <hawk.taishi0616@gmail.com>
2024-01-28 23:09:36 +0900

    修正

--- app/models/job_offer.rb
+++ app/models/job_offer.rb
@@ -9,8 +9,5 @@ class JobOffer < ApplicationRecord
   #
   # validations
   #
-  validates :occupation_type, presence: true
-
-  enum occupation_type: { backend: 0, web_frontend: 1, game_client: 2, devops: 3, qa: 4, data_engineer: 5, data_scientist: 6, security: 7, mobile: 8, machine_learning: 9,
-                          infla: 10 }
+  
 end
