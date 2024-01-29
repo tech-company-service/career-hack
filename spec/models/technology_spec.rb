736609f30431666186929d8705ba95ca8cfa8400
hawk0616 <hawk.taishi0616@gmail.com>
2024-01-29 16:12:50 +0900

    修正

--- spec/models/technology_spec.rb
+++ spec/models/technology_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe Technology, type: :model do
 
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_uniqueness_of(:name) }
-    it { is_expected.to validate_presence_of(:description) }
   end
 
   describe 'Associations' do
