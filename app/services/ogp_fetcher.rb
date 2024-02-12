276108f9b89eb7a140090e08897fb15b7239adce
hawk0616 <hawk.taishi0616@gmail.com>
2024-02-12 20:53:46 +0900

    修正

--- app/services/ogp_fetcher.rb
+++ app/services/ogp_fetcher.rb
@@ -8,13 +8,11 @@ class OgpFetcher
     description = page.description
     url = page.url
     image_url = page.meta['og:image']
-    favicon_url = page.images.favicon
 
     { title: title,
       description: description,
       url: url,
       image_url: image_url,
-      favicon_url: favicon_url
     }
   rescue StandardError => e
     return e.message
