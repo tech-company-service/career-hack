40fdff2a888adca939e420ec98cfbf03ad659b98
Daichi-appy <daichi.es47@gmail.com>
2024-07-28 01:37:07 +0900

    user関連API

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,40 @@
 class Api::V1::UsersController < ApplicationController
+  def show
+    user = User.find_by(uid: params[:uid])
+
+    if user
+      render json: user, include: %w[industries company_selection_preferences technologies]
+    else
+      render json: { error: 'ユーザーが見つかりませんでした' }, status: :not_found
+    end
+  end
+
   def create
     user = User.find_or_create_by!(provider: params[:provider], uid: params[:uid], name: params[:name], email: params[:email])
     head :ok if user
   rescue StandardError => e
     render json: { error: e.message }, status: :internal_server_error
+  end
+
+  def update
+    user = User.find_by(uid: params[:uid])
+    if user
+      user.update!(user_params)
+      head :ok
+    else
+      render json: { error: '更新できませんでした' }, status: :not_found
+    end
+  rescue StandardError => e
+    render json: { errors: { displayed_message: '更新できませんでした', error_message: e.message } }, status: :internal_server_error
+  end
+
+  private
+
+  def user_params
+    params.permit(
+      :name, :email, :nick_name, :graduate_year, :target_position, :introduction,
+      :x_url, :qiita_url, :zenn_url, :github_url, :wantedly_url, :other_url,
+      :research_content, :career_vision
+    )
   end
 end
