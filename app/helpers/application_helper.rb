8c37c6653a1118117d6d9c7e666d5ca1953380fb
Daichi-appy <daichi.es47@gmail.com>
2024-11-15 23:06:25 +0900

    詳細ページ関連情報表示まで

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,22 @@
+module ApplicationHelper
+  def format_date(date_string)
+    return '-' if date_string.nil? || date_string.empty?
+
+    case date_string.length
+    when 4
+      "#{date_string}年"
+    when 6
+      match = date_string.match(/(\d{4})(\d{2})/)
+      raise ArgumentError, 'Invalid date format for year and month' unless match
+
+      "#{match[1]}年#{match[2].to_i}月"
+    when 8
+      match = date_string.match(/(\d{4})(\d{2})(\d{2})/)
+      raise ArgumentError, 'Invalid date format for year, month, and day' unless match
+
+      "#{match[1]}年#{match[2].to_i}月#{match[3].to_i}日"
+    else
+      '-'
+    end
+  end  
+end
